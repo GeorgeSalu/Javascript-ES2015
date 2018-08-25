21e64440616c776d62f47d455607de7ea8e49241
George Salu <george.salu10@gmail.com>
2018-08-25 16:47:27 -0300

    Introdução ao Javascript - Basico |

---
+++ JSBasico/index.js
@@ -0,0 +1,13 @@
+let a = 10;
+const b = 20;
+
+console.log(a == b);
+
+//condicional
+let cor = "verde";
+
+if(cor === "verde") {
+    console.log("siga");
+} else {
+    console.log("pare");
+}
---
+++ JSBasico/index2.js
@@ -0,0 +1,15 @@
+let cor = "azul";
+
+switch (cor) {
+    case "verde":
+        console.log("siga");
+        break;
+
+    case "amarelo":
+        console.log("siga");
+        break;
+    
+    case "vermelho":
+        console.log("siga");
+        break;
+}
