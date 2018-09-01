b63900a9bbbb8fcc22362fba1449fb831467fdb7
George Salu <george.salu10@gmail.com>
2018-08-31 21:15:53 -0300

    correções de bugs com o ponto

--- projeto-calculadora-clone/scripts/controller/CalcController.js
+++ projeto-calculadora-clone/scripts/controller/CalcController.js
@@ -171,7 +171,7 @@ class CalcController {
     addDot() {
         let lastOperation = this.getLastOperation();
 
-        if(lastOperation && lastOperation.split('').indexOf('.') > -1) return;
+        if(typeof lastOperation === string && lastOperation.split('').indexOf('.') > -1) return;
 
         if(this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.');
