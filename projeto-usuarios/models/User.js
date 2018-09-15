d65a1361b273bc2fdac0cc33301422f8d9f8ac01
George Salu <george.salu10@gmail.com>
2018-09-15 12:12:44 -0300

    trabalhano com localStorage - Correção com Object.assign

--- projeto-usuarios/models/User.js
+++ projeto-usuarios/models/User.js
@@ -95,8 +95,8 @@ class User {
 		if(this.id > 0) {
 	
 			users.map(u => {
-				if(u._id === this.id) {
-					u = this;
+				if(u._id == this.id) {
+					Object.assign(u, this);
 				}
 				return u;
 			})
@@ -107,7 +107,7 @@ class User {
 			users.push(this);
 	
 		}
-		localStorage.setItem("users", JSON.stringify(users));
+		localStorage.setItem("user", JSON.stringify(users));
 
 	}
 
