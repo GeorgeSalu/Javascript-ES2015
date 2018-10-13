7db0f6bb58012f5ad660bcfc41816fd35e7ebfcf
George Salu <george.salu10@gmail.com>
2018-10-13 14:10:19 -0300

    navegando nas pastas ou abrindo arquivo

--- projeto-dropbox-clone/app/public/src/controller/DropBoxController.js
+++ projeto-dropbox-clone/app/public/src/controller/DropBoxController.js
@@ -4,9 +4,10 @@ class DropBoxController {
 
         this.currentFolder = ['hcode'];
 
-        this.navEl = document.querySelector('#browser-location');
-
         this.onselectionchange = new Event('selectionchange');
+
+        this.navEl = document.getElementById('browse-location');
+
 
         this.btnSendFile1 = document.querySelector('#btn-send-file');
         this.inputFileEl = document.querySelector('#files');
@@ -473,6 +474,49 @@ class DropBoxController {
 
     renderNav() {
         
+        let nav = document.createElement('nav');
+        let path = [];
+
+        for(let i = 0; i < this.currentFolder.length;i++) {
+
+            let folderName = this.currentFolder[i];
+            let span = document.createElement('span');
+
+            path.push(folderName);
+
+            if((i+1) === this.currentFolder.length) {
+                span.innerHTML = folderName;
+            } else {
+
+                span.className = 'breadcrumb-segment__wrapper';
+
+                span.innerHTML = `
+                                <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                                    <a href="#" data-path="${path.join('/')}" class="breadcrumb-segment">${folderName}</a>
+                                </span>
+                                <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                                    <title>arrow-right</title>
+                                    <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
+                                </svg>
+                `;
+
+            }
+            nav.appendChild(span);
+        }
+
+        this.navEl.innerHTML = nav.innerHTML;
+
+        this.navEl.querySelectorAll('a').forEach(a => {
+            a.addEventListener('click', e => {
+                e.preventDefault();
+
+                this.currentFolder = a.dataset.path.split('/');
+
+                this.openFolder();
+
+            })
+        })
+
     }
 
     initEventsLi(li) {
