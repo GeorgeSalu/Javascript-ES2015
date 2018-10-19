a7e74e971764a84006adfbff5b7493b415227bb2
George Salu <george.salu10@gmail.com>
2018-10-19 19:25:44 -0300

    Eventos no Menu Anexar

--- projeto-whatsapp-clone/src/controller/WhatsAppController.js
+++ projeto-whatsapp-clone/src/controller/WhatsAppController.js
@@ -109,7 +109,7 @@ class WhatsAppController {
 
         })
 
-        this.el.btClosePanelAddContact.on('click', e => {
+        this.el.btnClosePanelAddContact.on('click', e => {
             this.el.panelAddContact.removeClass('open');
         })
 
@@ -124,16 +124,103 @@ class WhatsAppController {
             }
         });
 
-        this.this.el.btnSavePanelEditProfile.on('click', e => {
+        this.el.btnSavePanelEditProfile.on('click', e => {
 
         });
 
         this.el.formPanelAddContact.on('submit', e => {
             e.preventDefault();
 
+            let formData = new FormData(this.el.formPanelAddContact);
 
+        });
+
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
+
+            item.on('click', e => {
+
+                this.el.main.css({
+                    display: 'flex'
+                });
+
+            })
+
+        });
+
+        this.el.btnAttach.on('click', e => {
+
+            e.stopPropagation();
+            this.el.menuAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+
+        });
+
+        this.el.btnAttachPhoto.on('click', e => {
+            this.el.inputPhoto.click();
+        });
+
+        this.el.inputPhoto.on('change', e => {
+
+            [...this.el.inputPhoto.files].forEach(file => {
+                console.log(file);
+            });
+
+        });
+
+        this.el.btnAttachCamera.on('click', e => {
+            this.el.panelMessagesContainer.hide();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height': 'calc(100% - 120px)'
+            })
+        });
+
+        this.el.btnClosePanelCamera.on('click', e => {
+            this.el.panelCamera.removeClass('open');
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnTakePicture.on('click', e => {
+            console.log('take pictures');
         })
 
+        this.el.btnAttachDocument.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                'height': 'calc(100% - 120px)'
+            })
+        });
+
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        })
+
+        this.el.btnSendDocument.on('click', e => {
+            console.log('send document');
+        })
+
+        this.el.btnAttachContact.on('click', e => {
+            this.el.modalContacts.show();
+        });
+
+        this.el.btnCloseModalContacts.on('click', e => {
+            this.el.modalContacts.hide();
+        })
+
+    }
+
+    closeAllMainPanel() {
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
+    }
+
+    closeMenuAttach(e) {
+        document.removeEventListener('click', this.closeMenuAttach);
+        this.el.menuAttach.removeClass('open');
+        console.log('remove menu');
     }
 
     closeAllLeftPanel() {
