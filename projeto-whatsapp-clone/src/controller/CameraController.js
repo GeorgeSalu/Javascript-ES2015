ca957421bd2ae4184e887732e3cf73c14111f074
George Salu <george.salu10@gmail.com>
2018-10-21 12:55:36 -0300

    parando de capturar imagem da camera - getTracks()

--- projeto-whatsapp-clone/src/controller/CameraController.js
+++ projeto-whatsapp-clone/src/controller/CameraController.js
@@ -7,6 +7,7 @@ export class CameraController {
             video: true
         }).then(stream => {
 
+            this._stream = stream;
             this._videoEl.src = URL.createObjectURL(stream);
             this._videoEl.play();
 
@@ -14,6 +15,12 @@ export class CameraController {
 
         });
 
+    }
+
+    stop(){
+        this._stream.getTracks().forEach(track => {
+            track.stop();
+        });
     }
 
 }
