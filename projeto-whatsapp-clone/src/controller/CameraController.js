c004ddd7df6c514c7fa7af9b3722fbd531effe48
George Salu <george.salu10@gmail.com>
2018-10-23 18:14:20 -0300

    tirando fotografia da camera

--- projeto-whatsapp-clone/src/controller/CameraController.js
+++ projeto-whatsapp-clone/src/controller/CameraController.js
@@ -23,4 +23,18 @@ export class CameraController {
         });
     }
 
+    takePicture(mimeType = 'image/png') {
+
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('height', this._videoEl.videoHeight);
+        canvas.setAttribute('width', this._videoEl.videoWidth);
+
+        let contex = canvas.getContext('2d');
+
+        contex.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
+
+        return canvas.toDataURL(mimeType);
+    }
+
 }
