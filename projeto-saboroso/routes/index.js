cd2cb38ef3f7ff6f8c7324b3ef620d1d1d33627d
George Salu <george.salu10@gmail.com>
2019-09-12 12:10:43 -0300

    exibindo dados do banco com ejs

--- projeto-saboroso/routes/index.js
+++ projeto-saboroso/routes/index.js
@@ -1,9 +1,23 @@
+var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Restaurante Saboroso!' });
+
+  conn.query(`select * from tb_menus order by title`, (err, results) => {
+    if(err) {
+      console.log(err);
+    }
+    console.log("result "+results)
+    res.render('index', {
+       title: 'Restaurante Saborosos!',
+       menus: results 
+    });
+
+  })
+
+  
 });
 
 module.exports = router;
